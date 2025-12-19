7bed855f155611e3f3f3d852ea991d44f57561c6
Basistrain <dylanmorel0117@gmail.com>
2025-12-19 15:18:46 +0000

    depot

--- Terraform/scripts/startup.sh
+++ Terraform/scripts/startup.sh
@@ -13,20 +13,20 @@ apt-get install php-curl php-gd php-mbstring php-xml php-xmlrpc -y
 systemctl start apache2
 # Enable Apache to start on boot
 systemctl enable apache2
-
-rm -rf /var/www/html/*
-cd /tmp
+cd /var/www/html
+rm -rf *
+# Download and set up a sample web page (WordPress)
 wget https://wordpress.org/latest.zip
 unzip latest.zip
-mv wordpress/* /var/www/html/
+mv wordpress/* .
 rm -rf wordpress latest.zip
 
-cp wp-config-sample.php /var/www/html/wp-config.php
+cp wp-config-sample.php wp-config.php
 # Set database connection details
-sed -i "s/database_name_here/${db_name}/" /var/www/html/wp-config.php
-sed -i "s/username_here/${db_user}/" /var/www/html/wp-config.php
-sed -i "s/password_here/${db_password}/" /var/www/html/wp-config.php
-sed -i "s/localhost/${db_host}/" /var/www/html/wp-config.php
+sed -i "s/database_name_here/${db_name}/" wp-config.php
+sed -i "s/username_here/${db_user}/" wp-config.php
+sed -i "s/password_here/${db_password}/" wp-config.php
+sed -i "s/localhost/${db_host}/" wp-config.php
 
 # Set ownership and permissions for the web server's root directory
 chown -R www-data:www-data /var/www/html

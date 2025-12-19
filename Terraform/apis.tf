ef3659b966a547a2deeb22d302309e95ff97e293
Basistrain <dylanmorel0117@gmail.com>
2025-12-19 15:01:51 +0000

    depot

--- Terraform/apis.tf
+++ Terraform/apis.tf
@@ -3,3 +3,15 @@ resource "google_project_service" "services" {
   project = "TP-Terraform-Micsi"
   service = each.value
 }
+resource "google_project_iam_member" "service-accounts" {
+  for_each = toset(var.service_accounts)
+  project = "TP-Terraform-Micsi"
+  role    = each.value.role
+  member  = "serviceAccount:${each.value.email}"
+}
+resource "google_project_iam_member" "project-roles" {
+  for_each = toset(var.project_roles)
+  project = "TP-Terraform-Micsi"
+  role    = each.value.role
+  member  = each.value.member
+}
